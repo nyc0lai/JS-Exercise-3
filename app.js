48900c65a3e4620f476cd16fadac86232d053d71
nyc0lai <nyc0lai@gmail.com>
2021-03-18 17:19:10 +0200

    create a index.html and app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+let userName = 'Gheorghe Zama';
+
+let userQuestion =  "Cind vine ursu?";
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = "";
+
+
+
+userName ? console.log(`Hello, ${userName}!`): console.log('Hello!');
+
+console.log(`${userQuestion} dear ${userName}`);
+//console.log(userName);
